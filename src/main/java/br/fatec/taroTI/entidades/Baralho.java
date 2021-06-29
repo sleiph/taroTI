23d2d1df304742d7dfacc4dbaecb3ac0e4fb0222
Ricardo Gomes <sleiph@gmail.com>
2021-06-29 17:20:12 -0300

    adicionando uma carta

--- src/main/java/br/fatec/taroTI/entidades/Baralho.java
+++ src/main/java/br/fatec/taroTI/entidades/Baralho.java
@@ -15,7 +15,8 @@ public class Baralho {
             new Carta("Às de Paus", "Tá criando várias paradas.", "Relaxa aí ô Artistão.", "paus/01"),
             new Carta("IX de Paus", "Depressão.", "Depressão pra caralho!", "paus/09"),
             new Carta("Às de Copos", "Vai começar o amor.", "Vai começar de cafonice.", "copos/01"),
-            new Carta("II de Copos", "Vai achar alguém.", "Vai perder alguém.", "copos/02")
+            new Carta("II de Copos", "Vai achar alguém.", "Vai perder alguém.", "copos/02"),
+            new Carta("VII de Copos", "Parece que vai dar tudo certo. Parece...", "Investiu em bitcoin?", "copos/07")
     };
 
     public Baralho() {
